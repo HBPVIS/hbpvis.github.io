c60cfa329f8a15b55f813201fcf8c94e1d7ddcc1
BBP Release Manager <bbprelman@epfl.ch>
2017-01-24 01:36:00 +0100

    Updated ZeroEQ 0.8

--- ZeroEQ-0.8/classzeroeq_1_1_version.js
+++ ZeroEQ-0.8/classzeroeq_1_1_version.js
@@ -5,7 +5,7 @@ var classzeroeq_1_1_version =
     [ "getMajor", "classzeroeq_1_1_version.html#a3922d590d921556db3364f342a9de35b", null ],
     [ "getMinor", "classzeroeq_1_1_version.html#ac9bf2336e9766d3b4932ec24358d9fe3", null ],
     [ "getPatch", "classzeroeq_1_1_version.html#a0b6fe9dd2d568fd46dda4d1a6b6e3756", null ],
-    [ "getRevision", "classzeroeq_1_1_version.html#a84fedf5ae233349cc115bd6560359046", null ],
+    [ "getRevision", "classzeroeq_1_1_version.html#ae8a952c38ba0fee98bd646ec8ae1ca9c", null ],
     [ "getRevString", "classzeroeq_1_1_version.html#a767af1de0d4f77d505364bcb5b299714", null ],
     [ "getSchema", "classzeroeq_1_1_version.html#ad726187da238bfa38b950e8968bce36d", null ],
     [ "getString", "classzeroeq_1_1_version.html#a08be1ee112c6892da56d26dceb208d32", null ],
--- _projects/ZeroEQ-0.8.md
+++ _projects/ZeroEQ-0.8.md
@@ -4,7 +4,7 @@ version: "0.8"
 major: 0
 minor: 8
 description: ZeroEQ - Zero Event Queue
-updated: 07/01/17
+updated: 24/01/17
 homepage: https://github.com/HBPVIS/ZeroEQ
 repository: https://github.com/HBPVIS/ZeroEQ.git
 issuesurl: https://github.com/HBPVIS/ZeroEQ/issues
@@ -12,7 +12,7 @@ packageurl:
 license: LGPL
 maturity: EP
 maintainers: Human Brain Project (HBPVis@googlegroups.com)
-contributors: Daniel Nachbaur
+contributors: Stefan Eilemann
 readmetype: text/x-markdown
 ---
 [TOC]
